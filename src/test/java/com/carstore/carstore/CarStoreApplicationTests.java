f5a69e5de65a1b2c0721347551f03e952fddd83a
aquajack22 <aquajack22@gmail.com>
2021-03-18 10:32:06 +0100

    Created the project structure for spring boot backend

---
+++ src/test/java/com/carstore/carstore/CarStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carstore.carstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
